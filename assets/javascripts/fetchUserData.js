e1f98ec1b24f9ad18980e34de8e4eef88ba2c246
Team SaaSBox <team@saasbox.net>
2023-10-06 20:04:32 -0700

    Update admin pug and iframe to submit prompt content to localhost
    localhost:5000 has a python service

--- assets/javascripts/fetchUserData.js
+++ assets/javascripts/fetchUserData.js
@@ -1,4 +1,34 @@
 
+
+// Submits chatgpt prompt.
+// Expected result is current page is updated.
+// Next up: ideally will reload the page in 20 seconds or so.
+//
+const submitPrompt = async function() {
+  let jwt = $("#token").attr("data-token");
+  let user_prompt = $("textInput").val();
+  let server_url = "http://localhost:5000";
+  return new Promise((resolve, reject) => {
+    $.ajax({
+        url: server_url + "/create-get-user",
+        headers: { Authorization: "Bearer " + jwt },
+        contentType: 'application/json; charset=utf-8',
+        dataType: 'json',
+        data: JSON.stringify({ user_prompt, page_name: "admin.pug" }),
+        type: 'POST',
+        success: ((res) => {
+            let result = res.obj;
+            console.log("Prompt submission result:", result);
+            resolve(result);
+        }),
+        error: ((err) => {
+            console.log("Error submitting prompt: ", err.responseJSON.error);
+            reject({ error: err.responseJSON.error });
+        })
+    });
+  });
+}
+
 const createGetUser = async function() {
   let jwt = $("#token").attr("data-token");
   let server_url = "https://expressjs-api-backend.onrender.com";
